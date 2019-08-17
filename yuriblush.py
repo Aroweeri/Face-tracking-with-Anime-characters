6c5d24ab49f1c72ed913616dc01c5166bd0dee53
Dylan Sutton <dylanlsutton@gmail.com>
2019-08-17 05:34:53 -0400

    Consolidate all non-indented code into a main function.

--- yuriblush.py
+++ yuriblush.py
@@ -5,17 +5,15 @@ import ctypes
 import time
 import pygame
 
-pygame.init()
 black = (255,255,255)
 ix = 960
 iy = 960
-
 scl=1600
 scb=900
+ge = None
+eye = None
+kb = None 
 
-ge = pygame.image.load('Yuri cute.png')
-eye = pygame.image.load('Yuriceyes.png')
-kb = pygame.image.load('Yuri blush.png')
 def pos(gameDisplay,ge,x,y):
 	gameDisplay.blit(ge,(x,y))
 
@@ -62,20 +60,7 @@ def show_yuri_image(w, h):
 	return gameDisplay
 
 
-print("Width: ",scl,"Height: ",scb) #screen metrics
 
-camera =  cv2.VideoCapture(0)
-
-w = camera.set(3, scl/2)
-h = camera.set(4, scb/2)
-face = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-fps = camera.get(cv2.CAP_PROP_FPS)
-print("fps: ",fps)
-settings = {
-	'scaleFactor': 1.3, 
-	'minNeighbors': 3, 
-	'minSize': (50, 50), 
-}
 
 def rotate_image(image, angle):
 	if angle == 0: return image
@@ -92,39 +77,67 @@ def rotate_point(pos, img, angle):
 	newy = -x*sin(radians(angle)) + y*cos(radians(angle)) + img.shape[0]*0.4
 	return int(newx), int(newy), pos[2], pos[3]
 
-gameDisplay = show_yuri_image(scl, scb)
-running = True
-while running:
-	event = pygame.event.wait ()
-	pressed = pygame.key.get_pressed()
-	if(pressed[pygame.K_q]):
-		running = False
+def main():
 
-	ret, imgn = camera.read()
-	img = cv2.flip(imgn, +1)
+	global ge
+	global eye
+	global kb
 
-	for angle in [0, -25, 25]:
-		rimg = rotate_image(img, angle)
-		detected = face.detectMultiScale(rimg, **settings)
-		if len(detected):
-			detected = [rotate_point(detected[-1], img, -angle)]
+	pygame.init()
+	ge = pygame.image.load('Yuri cute.png')
+	eye = pygame.image.load('Yuriceyes.png')
+	kb = pygame.image.load('Yuri blush.png')
+
+	print("Width: ",scl,"Height: ",scb) #screen metrics
+
+	camera =  cv2.VideoCapture(0)
+
+	w = camera.set(3, scl/2)
+	h = camera.set(4, scb/2)
+	face = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+	fps = camera.get(cv2.CAP_PROP_FPS)
+	print("fps: ",fps)
+	settings = {
+		'scaleFactor': 1.3, 
+		'minNeighbors': 3, 
+		'minSize': (50, 50), 
+	}
+
+	gameDisplay = show_yuri_image(scl, scb)
+	running = True
+	while running:
+		event = pygame.event.wait ()
+		pressed = pygame.key.get_pressed()
+		if(pressed[pygame.K_q]):
+			running = False
+
+		ret, imgn = camera.read()
+		img = cv2.flip(imgn, +1)
+
+		for angle in [0, -25, 25]:
+			rimg = rotate_image(img, angle)
+			detected = face.detectMultiScale(rimg, **settings)
+			if len(detected):
+				detected = [rotate_point(detected[-1], img, -angle)]
+				break
+
+		# Make a copy as we don't want to draw on the original image:
+		for x, y, w, h in detected[-1:]:
+			cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+			xcord = (x+w)/2
+			ycord = 2*(y+h)/3 # to look at the eyes
+			# put in else move_eyes(gameDisplay, xcord, ycord,)
+			if x+w >400 and y+h >400:
+				print("too close")
+				blush(gameDisplay,kb)
+			else:
+				move_eyes(gameDisplay, xcord, ycord,)
+		#cv2.imshow('facedetect', img)
+		if cv2.waitKey(5) != -1:
 			break
 
-	# Make a copy as we don't want to draw on the original image:
-	for x, y, w, h in detected[-1:]:
-		cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
-		xcord = (x+w)/2
-		ycord = 2*(y+h)/3 # to look at the eyes
-		# put in else move_eyes(gameDisplay, xcord, ycord,)
-		if x+w >400 and y+h >400:
-			print("too close")
-			blush(gameDisplay,kb)
-		else:
-			move_eyes(gameDisplay, xcord, ycord,)
-	#cv2.imshow('facedetect', img)
-	if cv2.waitKey(5) != -1:
-		break
+	cv2.destroyWindow("facedetect")
+	pygame.quit()
+	quit()
 
-cv2.destroyWindow("facedetect")
-pygame.quit()
-quit()
+main()
